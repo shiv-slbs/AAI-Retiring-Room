9925229db407ee19d190a0183853af4e99f1f3d4
HARSH SONI <harshsoni6411@gmail.com>
2025-06-29 14:17:05 +0530

    user.js

--- models/user.js
+++ models/user.js
@@ -1,9 +1,21 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-  fullName: String,
-  mobileNo: String,
-  emailId: String,
+const userSchema = new mongoose.Schema({
+  fullname: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  mobile: {
+    type: String,
+    required: true,
+    match: /^[0-9]{10}$/
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true
+  }
 });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', userSchema);
