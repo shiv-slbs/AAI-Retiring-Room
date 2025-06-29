70bca75be5b3f7cddd24757aaadf758c4c781be5
HARSH SONI <harshsoni6411@gmail.com>
2025-06-29 14:15:36 +0530

    Booking.js

--- models/Booking.js
+++ models/Booking.js
@@ -1,15 +1,56 @@
 const mongoose = require('mongoose');
 
-const BookingSchema = new mongoose.Schema({
-  fullName: String,
-  mobileNo: String,
-  emailId: String,
-  arrivalFlightNo: String,
-  departureFlightNo: String,
-  checkInDate: String,
-  checkOutDate: String,
-  roomType: String,
-  totalStayHours: Number,
+const bookingSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  arrivalFlightNo: {
+    type: String,
+    required: true
+  },
+  departureFlightNo: {
+    type: String,
+    required: true
+  },
+  pnr: {
+    type: String,
+    required: true
+  },
+  checkinDate: {
+    type: Date,
+    required: true
+  },
+  checkoutDate: {
+    type: Date,
+    required: true
+  },
+  roomType: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Room',
+    required: true
+  },
+  startHour: {
+    type: Number,
+    required: true
+  },
+  endHour: {
+    type: Number,
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  gst: {
+    type: Number,
+    required: true
+  },
+  totalAmount: {
+    type: Number,
+    required: true
+  }
 });
 
-module.exports = mongoose.model('Booking', BookingSchema);
+module.exports = mongoose.model('Booking', bookingSchema);
